185e89ce708fa284ea60474c2bf886965985d043
ibonetc <isisbcruz@gmail.com>
2024-11-21 12:54:44 -0500

    Add files via upload

---
+++ PruebaCamara.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+
+#conda install -c menpo opencv
+#pip install opencv-python
+
+import cv2 # opencv
+
+########Probar cámara
+video_capture = cv2.VideoCapture(0)
+while(True):
+    ret, frame = video_capture.read()
+    cv2.imshow('frame', frame)  # Mostrar el frame capturado con la cámara
+    if cv2.waitKey(1) & 0xFF == 27: #detener con tecla Esc
+        break
+video_capture.release()
+cv2.destroyAllWindows()
+
